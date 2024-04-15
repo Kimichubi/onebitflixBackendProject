1a29f5b582b5fb513c9ad2e0181265617ebbaa5a
Kimas <gabrielpoyta@outlook.com>
2024-04-15 13:59:52 -0300

    Configuração de dependencias, postgres, typescript, express e instalação das dependencias.

---
+++ config/sequelizeCli.js
@@ -0,0 +1,12 @@
+module.exports = {
+  development: {
+    dialect: "postgres",
+    host: "localhost",
+    port: "5432",
+    database: "onebitflix_development",
+    //Database connection settings for development, after upgrade to production
+    username: "onebitflix",
+    password: "onebitflix",
+  },
+};
+''
