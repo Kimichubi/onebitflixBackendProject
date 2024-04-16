c7c1395156a2c3584daf511bffcb0c7aab7c534f
Kimas <gabrielpoyta@outlook.com>
2024-04-16 10:44:54 -0300

    Add AdminJS configs and some style
    Nesta parte do projeto foi adicionado algum estilo para a página admin, conectamos a database com o AdminJs, adicionamos o caminho roothPath, criamos a rota, usamos o express para conectar com o AdminJS

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
 import express from "express";
 import { sequelize } from "./database";
+import { adminJs, adminJsRouter } from "./adminjs";
 
 const app = express();
 
+//express app.use(caminho, rotas)
+//localhost:3000/admin
+app.use(express.static('public'))
+app.use(adminJs.options.rootPath, adminJsRouter)
+
 const PORT = process.env.PORT || 3000;
+
+
 
 app.listen(PORT, () => {
    sequelize.authenticate().then(() => {
