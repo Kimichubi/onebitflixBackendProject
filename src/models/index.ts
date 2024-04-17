32b1eafb3ca1eb993570539b2597bb93874788b5
Kimas <gabrielpoyta@outlook.com>
2024-04-17 11:24:57 -0300

    Criação tabela de episodios
    Novamente migration de episodios, resources e models

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,10 +1,18 @@
-import {Category} from './Category'
+// src/models/index.ts
+
+import { Category } from './Category'
 import { Course } from './Course'
+import { Episode } from './Episode'
 
 Category.hasMany(Course)
-Course.belongsTo(Category)
 
-export{
-    Category,
-    Course
+Course.belongsTo(Category)
+Course.hasMany(Episode)
+
+Episode.belongsTo(Course)
+
+export {
+  Category,
+  Course,
+  Episode
 }
