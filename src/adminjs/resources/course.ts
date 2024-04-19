26cadb0ea1c0858813466e8b40a64f3a3775eb41
Kimas <gabrielpoyta@outlook.com>
2024-04-19 17:53:57 -0300

    Upload course thumb

--- src/adminjs/resources/course.ts
+++ src/adminjs/resources/course.ts
@@ -1,6 +1,8 @@
 // src/adminjs/resources/course.ts
 
-import { ResourceOptions } from "adminjs";
+import uploadFileFeature from "@adminjs/upload";
+import { FeatureType, ResourceOptions } from "adminjs";
+import path from "path";
 
 export const courseResourceOptions: ResourceOptions = {
   navigation: "Catálogo",
@@ -31,3 +33,18 @@ export const courseResourceOptions: ResourceOptions = {
     "updatedAt",
   ],
 };
+
+export const courseResourceFeatures: FeatureType[] = [
+  uploadFileFeature({
+    provider: {
+      local: {
+        bucket: path.join(__dirname, "..", "..", "..", "uploads"),
+      },
+    },
+    properties: {
+      key: "thumbnailUrl",
+      file: "uploadThumbnail",
+    },
+    uploadPath: (record , filename) => `thumbnails/course-${record.get('id')}/${filename}` 
+  }),
+];
