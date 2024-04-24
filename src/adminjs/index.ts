750953308b420a174533e7efe32511dc4ce4c28d
Kimas <gabrielpoyta@outlook.com>
2024-04-24 09:40:38 -0300

    Create auth and UI login

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -3,6 +3,8 @@ import AdminJSExpress from "@adminjs/express"
 import AdminJSSequelize from "@adminjs/sequelize"
 import { sequelize } from "../database";
 import { adminJsResources } from "./resources";
+import { User } from "../models";
+import bcrypt from "bcrypt"
 
 AdminJS.registerAdapter(AdminJSSequelize)
 
@@ -39,4 +41,21 @@ export const adminJs = new AdminJS({
     }
 )
 //The routers of AdminsJs we need in the express
-export const adminJsRouter = AdminJSExpress.buildRouter(adminJs)
+export const adminJsRouter = AdminJSExpress.buildAuthenticatedRouter(adminJs, {
+  authenticate : async (email, password) => {
+     const user = await User.findOne({ where : {email : email} })
+
+     if(user && user.role === 'admin'){
+      const matched = await bcrypt.compare(password, user.password)
+      if(matched){
+        return user
+      }
+
+      return false
+     }
+  },
+  cookiePassword : "senha-de-cookie"
+}, null,{
+  resave : false,
+  saveUninitialized : false
+})
