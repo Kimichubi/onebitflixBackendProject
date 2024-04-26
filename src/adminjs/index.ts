46594ff1b3d944bd6aedb363dc407e496b591891
Kimas <gabrielpoyta@outlook.com>
2024-04-26 14:02:45 -0300

    Newest courses

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -1,6 +1,6 @@
 import AdminJS from "adminjs";
-import AdminJSExpress from "@adminjs/express"
-import AdminJSSequelize from "@adminjs/sequelize"
+import AdminJSExpress from "@adminjs/express";
+import AdminJSSequelize from "@adminjs/sequelize";
 import { sequelize } from "../database";
 import { adminJsResources } from "./resources";
 import { locale } from "./locale";
@@ -8,25 +8,28 @@ import { dashboardOptions } from "./dashboard";
 import { brandingOptions } from "./branding";
 import { authenticationOptions } from "./authentication";
 
-AdminJS.registerAdapter(AdminJSSequelize)
+AdminJS.registerAdapter(AdminJSSequelize);
 
 export const adminJs = new AdminJS({
-    //Admin connect to database
-    databases: [sequelize],
-    //The path
-    rootPath: "/admin" ,
+  //Admin connect to database
+  databases: [sequelize],
+  //The path
+  rootPath: "/admin",
 
-    resources: adminJsResources, 
-    
-    //Interface modifier
-    branding: brandingOptions,
-      locale : locale,
-      dashboard: dashboardOptions
-    }
-)
+  resources: adminJsResources,
+
+  //Interface modifier
+  branding: brandingOptions,
+  locale: locale,
+  dashboard: dashboardOptions,
+});
 //The routers of AdminsJs we need in the express
-export const adminJsRouter = AdminJSExpress.buildAuthenticatedRouter(adminJs, 
-  authenticationOptions , null,{
-  resave : false,
-  saveUninitialized : false
-})
+export const adminJsRouter = AdminJSExpress.buildAuthenticatedRouter(
+  adminJs,
+  authenticationOptions,
+  null,
+  {
+    resave: false,
+    saveUninitialized: false,
+  }
+);
