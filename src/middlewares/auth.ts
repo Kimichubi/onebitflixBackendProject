fa4cf8aefe383caf4bb39d637b9b8d5a0a865f5a
Kimas <gabrielpoyta@outlook.com>
2024-05-09 13:56:01 -0300

    Protecting the videos

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,14 +1,18 @@
 import { Response, Request, NextFunction } from "express";
 import { jwtService } from "../services/jwtService";
 import { userService } from "../services/userService";
-import { JwtPayload } from "jsonwebtoken";
+import { Jwt, JwtPayload } from "jsonwebtoken";
 import { UserInstance } from "../models/User";
 
 export interface AuthenticatedRequest extends Request {
-    user?: UserInstance | null
+  user?: UserInstance | null;
 }
 
-export function ensureAuth(req: AuthenticatedRequest, res: Response, next: NextFunction) {
+export function ensureAuth(
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) {
   const authorizationHeader = req.headers.authorization;
 
   if (!authorizationHeader)
@@ -18,15 +22,42 @@ export function ensureAuth(req: AuthenticatedRequest, res: Response, next: NextF
   // Bearer token is like this sihdashdsauhduhudasd
   const token = authorizationHeader.replace(/Bearer /, "");
 
-  jwtService.veryfyToken(token, (err, decoded) => {
+  jwtService.veryfyToken(token, async (err, decoded) => {
     if (err || typeof decoded === "undefined")
       return res
         .status(401)
         .json({ message: "Não autorizado: token inválido" });
 
-        userService.findByEmail((decoded as JwtPayload).email).then(user =>{
-            req.user = user
-            next()
-        })
+    const user = await userService.findByEmail((decoded as JwtPayload).email);
+    req.user = user;
+    next();
+  });
+}
+
+export function ensureAuthViaQuery(
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) {
+  const { token } = req.query;
+
+  if (!token)
+    return res
+      .status(401)
+      .json({ message: "Não autorizado: nenhum token encontrado" });
+
+  if (typeof token !== "string")
+    return res.status(400).json({
+      message: "O parâmetro token deve ser do tipo string",
+    });
+
+  jwtService.veryfyToken(token, async (err, decoded) => {
+    if (err || typeof decoded === "undefined")
+      return res.status(401).json({
+        message: "Não autorizado: token inválido",
+      });
+    const user = await userService.findByEmail((decoded as JwtPayload).email);
+    req.user = user
+    next()
   });
 }
